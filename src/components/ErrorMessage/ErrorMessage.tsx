d27539aa1feba8fa022b20f3a58a4206357e2550
andrei.gubsky <59491228+andreigubsky@users.noreply.github.com>
2026-01-26 16:34:18 +0200

    fix: remove unused types

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,11 +1,5 @@
-import css from "./ErrorMessage.module.css";
+import css from './ErrorMessage.module.css';
 
-interface ErrorMessageProps{
-
-}
-
-export default function ErrorMessage(){
-    return(
-        <p className={css.text}>There was an error, please try again...</p>
-    );
+export default function ErrorMessage() {
+  return <p className={css.text}>There was an error, please try again...</p>;
 }
