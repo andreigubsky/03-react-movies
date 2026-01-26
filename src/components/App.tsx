d1db817dd56fbb389771069583f08674c368f82e
andrei.gubsky <59491228+andreigubsky@users.noreply.github.com>
2026-01-26 12:06:18 +0200

    Add some stuff

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,16 @@
 import './App.module.css';
 import SearchBar from './SearchBar/SearchBar';
-// import fetchMovies from '../services/movieService';
-// import MovieGrid from './MovieGrid/MovieGrid';
-// import toast, { Toaster } from 'react-hot-toast';
+import MovieGrid from './MovieGrid/MovieGrid.tsч';
+import MovieModal from './MovieModal/MovieModal';
+import Loader from './Loader/Loader';
+import ErrorMessage from './ErrorMessage/ErrorMessage';
+
+import fetchMovies from '../services/movieService';
+import toast, { Toaster } from 'react-hot-toast';
+
+import { useState } from 'react';
+import { useEffect } from 'react';
+import type { Movie } from '../types/movie';
 //Якщо в результаті запиту масив фільмів порожній, виводьте повідомлення:
 
 // const notify = () => toast('Here is your toast.');
@@ -14,13 +22,21 @@ import SearchBar from './SearchBar/SearchBar';
 // }
 
 function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <>
-      <SearchBar onSubmit={() => {}} />
-      {/* <MovieGrid />
-      <Loader />
-      <ErrorMessage />
-      <MovieModal /> */}
+      <SearchBar
+        onSubmit={e => {
+          console.log(e);
+        }}
+      />
+      <MovieGrid movies={movies} onSelect={movie => console.log(movie)} />
+      {/* <MovieModal /> */}
+      {/* {isLoading && <Loader />} */}
+      {/* {isError && <ErrorMessage />} */}
     </>
   );
 }
